46f21ca6696aa6f5b7586505e53ebdabdf2a0377
shrishti188 <shrishtisharma59@gmail.com>
2025-07-27 21:17:42 +0530

    Remove venv from tracking

--- voice_command_shopping_assistant/settings.py
+++ voice_command_shopping_assistant/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'voice_command_shopping_assistant.urls'
